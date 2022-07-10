76ff692d8c9dd2ed5fc67b3fc015941a1daf9d94
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-07-11 02:18:29 +0900

    DB更新

--- reservation_calender.sql
+++ reservation_calender.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: localhost:8889
--- 生成日時: 2022 年 7 月 10 日 13:21
+-- 生成日時: 2022 年 7 月 10 日 17:17
 -- サーバのバージョン： 5.7.26
 -- PHP のバージョン: 7.4.2
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `reservation` (
   `id` int(11) NOT NULL,
   `name` varchar(11) NOT NULL,
-  `number` varchar(15) NOT NULL,
-  `member` int(11) NOT NULL,
+  `teacher_name` varchar(15) NOT NULL,
+  `time_number` int(11) NOT NULL,
   `day` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -81,7 +81,7 @@ ALTER TABLE `users`
 -- テーブルのAUTO_INCREMENT `reservation`
 --
 ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- テーブルのAUTO_INCREMENT `users`
