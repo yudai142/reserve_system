aa1e31cba8abd9218baa8e52c5e831c170c6750a
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-11-07 01:19:14 +0900

    prisma導入

---
+++ prisma/migrations/20221106161441_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "reservation" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(11) NOT NULL,
+    "teacher_name" VARCHAR(15),
+    "time_number" INTEGER NOT NULL,
+    "day" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "reservation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(11) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "modfied" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
