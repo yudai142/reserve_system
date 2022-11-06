32306e60aa7da11795a335b0c3fac799a006d3ae
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-11-06 21:52:43 +0900

    修正

--- dbconnect.php
+++ dbconnect.php
@@ -11,12 +11,16 @@ if (!file_exists($filename)){
   $dbc = substr($url["path"], 1);
   $dsn = 'pgsql:host=' . $host . ';dbname=' . $dbc . ';charset=utf8mb4';
 
-  $db = new PDO($dsn, $user, $pass, [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
-    ]
-  );
+  try {
+    $db = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+      ]
+    );
+  } catch(PDOException $e) {
+      print('DB接続エラー：' . $e->getMessage());
+  }
 }else{
   require_once 'env.php';
   // ini_set('display_errors', true);
@@ -25,7 +29,7 @@ if (!file_exists($filename)){
   $pass = DB_PASS;
   $dbc = DB_NAME;
   $dsn = "mysql:host=$host;dbname=$dbc;charset=utf8mb4";
-  
+
   try {
     $db = new PDO($dsn, $user, $pass, [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
