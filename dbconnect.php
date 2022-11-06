5528af87ef6ba98b17e269c9290b60d2f752745a
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-11-07 00:04:42 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -4,7 +4,6 @@ $filename = './env.php';
 if (!file_exists($filename)){
   //For Heroku
   $url = parse_url(getenv("DATABASE_URL"));
-  var_dump($url);
   try {
     $db = new PDO("pgsql:" . sprintf(
       "host=%s;port=%s;user=%s;password=%s;dbname=%s",
